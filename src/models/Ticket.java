5e2a015e04c3784ebe756823d7d05d21202baacd
Sahtani <soumia.sahtani@gmail.com>
2024-08-30 00:24:38 +0100

    add Ticket class with setters and getters

---
+++ src/models/Ticket.java
@@ -0,0 +1,84 @@
+package models;
+
+import java.sql.Date;
+import java.util.UUID;
+
+public class Ticket {
+
+    public enum TransportType {
+        AIR, BUS, RAIL, SEA
+    }
+
+    public enum TicketStatus {
+        PURCHASED, CANCELLED, REFUNDED, PENDING
+    }
+
+    private UUID id;
+    private TransportType transportType;
+    private float purchasePrice;
+    private float salePrice;
+    private Date saleDate;
+    private TicketStatus ticketStatus;
+
+    // Constructor
+    public Ticket(UUID id, TransportType transportType, float purchasePrice, float salePrice, Date saleDate, TicketStatus ticketStatus) {
+        this.id = id;
+        this.transportType = transportType;
+        this.purchasePrice = purchasePrice;
+        this.salePrice = salePrice;
+        this.saleDate = saleDate;
+        this.ticketStatus = ticketStatus;
+    }
+    //Getters
+
+    public UUID getId() {
+        return id;
+    }
+
+    public TransportType getTransportType() {
+        return transportType;
+    }
+
+    public float getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public float getSalePrice() {
+        return salePrice;
+    }
+
+    public Date getSaleDate() {
+        return saleDate;
+    }
+
+    public TicketStatus getTicketStatus() {
+        return ticketStatus;
+    }
+
+    //Setters
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setTransportType(TransportType transportType) {
+        this.transportType = transportType;
+    }
+
+    public void setPurchasePrice(float purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public void setSalePrice(float salePrice) {
+        this.salePrice = salePrice;
+    }
+
+    public void setSaleDate(Date saleDate) {
+        this.saleDate = saleDate;
+    }
+
+    public void setTicketStatus(TicketStatus ticketStatus) {
+        this.ticketStatus = ticketStatus;
+    }
+
+}
